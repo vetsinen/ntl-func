d5c8ad7d9f4d93cbd474df7b47b3e6e072e53551
vetsinen <vetsinen@gmail.com>
2023-08-25 09:21:38 +0300

    takes data from monga

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,4 +1,21 @@
-exports.handler = async () => ({
-    statusCode: 200,
-    body: 'boop',
-});
+const { MongoClient } = require("mongodb");
+
+const mongoClient = new MongoClient('mongodb+srv://superadmin:rbDkYE2K4Qir69Om@dvas-cluster0.o1qow.mongodb.net/');
+
+const clientPromise = mongoClient.connect();
+
+const handler = async (event) => {
+    try {
+        const database = (await clientPromise).db('main');
+        const collection = database.collection('ast');
+        const results = await collection.find({}).limit(10).toArray();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(results),
+        }
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() }
+    }
+}
+
+module.exports = { handler }
